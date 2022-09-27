bceba119104dacb96cda005cf6cf9f72ee47f8a0
Jinsujin <sylpid003@gmail.com>
2022-09-27 16:23:32 +0900

    DOS-2 feat: 탭바의 게임메뉴 터치시 present 로 뜨도록 함
    - UITabbar Controller 클래스 분리

--- DOS-Monster/DOS-Monster/SceneDelegate.swift
+++ DOS-Monster/DOS-Monster/SceneDelegate.swift
@@ -17,24 +17,46 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         UIFont.overrideInitialize()
         
-        let tabbarController = UITabBarController()
-        tabbarController.tabBar.backgroundColor = .systemGray4
-        tabbarController.tabBar.tintColor = .black
-        tabbarController.tabBar.isTranslucent = false
-        
-        let appearance = UITabBarItem.appearance()
-        let attributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20)]
-        appearance.setTitleTextAttributes(attributes as [NSAttributedString.Key: Any], for: .normal)
-        
         let mainViewController = ViewController()
         mainViewController.title = "Home"
         let gameViewController = GameViewController()
         gameViewController.title = "Game"
         
+        let tabbarController = RootTabBarController()
         tabbarController.setViewControllers([mainViewController, gameViewController], animated: false)
         
         window?.rootViewController = tabbarController
         window?.makeKeyAndVisible()
+    }
+}
+
+// MARK: - RootTabbar controller
+class RootTabBarController: UITabBarController, UITabBarControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        
+        setupTabbar()
+    }
+    
+    func setupTabbar() {
+        tabBar.backgroundColor = .systemGray4
+        tabBar.tintColor = .black
+        tabBar.isTranslucent = false
+        
+        let appearance = UITabBarItem.appearance()
+        let attributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20)]
+        appearance.setTitleTextAttributes(attributes as [NSAttributedString.Key: Any], for: .normal)
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController is GameViewController {
+            self.present(GameViewController(), animated: true)
+            return false
+        } else {
+            return true
+        }
     }
 }
 
